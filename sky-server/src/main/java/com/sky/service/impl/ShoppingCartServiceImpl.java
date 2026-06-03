5a48e87014a7ea8504334dee366a195655ce525f
mirror <git@mirror>
2026-06-03 15:33:26 +0800

    删除购物车item业务代码

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -42,7 +42,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             ShoppingCart cart = list.get(0);  //获取购物车数据
             cart.setNumber(cart.getNumber() + 1);
             shoppingCartMapper.updateNumberById(cart);  //更新
-        }else{
+        } else {
             //if 不存在，插入一条购物车数据
 
             //判断本次添加到购物车的是菜品还是套餐
@@ -77,6 +77,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     /**
      * 查看购物车
+     *
      * @return
      */
     @Override
@@ -100,6 +101,44 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         Long userId = BaseContext.getCurrentId();
 
         shoppingCartMapper.deleteByUserId(userId);
+
+    }
+
+    /**
+     * 删除购物车中的一件商品
+     *
+     * @param shoppingCartDTO
+     */
+    @Override
+    public void subShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
+        shoppingCart.setUserId(userId);
+
+        //查询，获取当前用户的购物车数据
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+
+        //判断购物车是否有数据
+        if(list != null && list.size() > 0){
+
+            shoppingCart = list.get(0);
+
+            Integer number = shoppingCart.getNumber();
+            if(number == 1){
+                //当前菜品有一份，直接删除
+                shoppingCartMapper.deleteById(shoppingCart.getId());
+            }else{
+                //当前菜品有多分，number--
+                shoppingCart.setNumber(shoppingCart.getNumber() - 1);
+                shoppingCartMapper.updateNumberById(shoppingCart);
+            }
+
+
+        }
+
+
 
     }
 }
